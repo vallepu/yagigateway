5754743a9b9630533bdb40b3224e733ef1e12812
niraj.pokharel@gmail.com <niraj.pokharel@gmail.com@e5acf81d-44b8-f241-cd30-7f371b5e9ff3>
2012-10-07 20:16:37 +0000

    BootStrap updated for default user admin creation

--- trunk/grails-app/conf/BootStrap.groovy
+++ trunk/grails-app/conf/BootStrap.groovy
@@ -1,6 +1,16 @@
+import com.yagi.paymentgateway.Role
+import com.yagi.paymentgateway.User
+import com.yagi.paymentgateway.UserRole
+
 class BootStrap {
 
     def init = { servletContext ->
+        def adminRole = Role.findByAuthority('ROLE_ADMIN') ?: new Role(authority: 'ROLE_ADMIN').save(failOnError: true)
+        def user = User.findByUsername('admin') ?: new User(username: 'admin',password: 'admin',enabled: true).save(failOnError: true)
+
+        if(!user.authorities.contains(adminRole)){
+            UserRole.create user, adminRole
+        }
     }
     def destroy = {
     }
