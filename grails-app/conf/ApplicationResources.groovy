20a203ab6ca92effd80fb5d649cefe5b5bef193d
niraj.pokharel <niraj.pokharel@gmail.com>
2012-10-08 19:42:34 +0000

    ApplicationResource CSS in core module added

--- grails-app/conf/ApplicationResources.groovy
+++ grails-app/conf/ApplicationResources.groovy
@@ -2,4 +2,8 @@ modules = {
     application {
         resource url:'js/application.js'
     }
+    core {
+        dependsOn 'jquery'
+        resource url:'/css/base.css',bundle: 'global'
+    }
 }
